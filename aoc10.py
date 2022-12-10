e566eca2b76a06ff26da87d47bd6c8edb8e1c7b4
Douglas Catchpole <douglas.catchpole@gmail.com>
2022-12-10 16:20:03 +1000

    day 10, nice and simple.

--- aoc10.py
+++ aoc10.py
@@ -1,7 +1,29 @@
 import sys
 
-answer = None
+answer = []
+ins = []
 for line in sys.stdin.readlines():
   line = line.rstrip()
+  ins.append(0)
+  if line.startswith('addx'):
+    v = int(line.split()[-1])
+    ins.append(v)
+
+screen = []
+X = 1
+for i, v in enumerate(ins):
+  if i % 40 == 19:
+   answer.append((i+1) * X)
+  if i % 40 == 0:
+    screen.append([])
+  if abs(X - (i % 40)) < 2:
+    screen[-1].append('#')
+  else:
+    screen[-1].append('.')
+
+  X += v 
+    
 
 print(answer)
+print(sum(answer))
+print('\n'.join([''.join(l) for l in screen]))
