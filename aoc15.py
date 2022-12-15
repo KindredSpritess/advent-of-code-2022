54fa977aa5e4507701335f0c8cf8b78676e586d5
Douglas Catchpole <>
2022-12-15 17:26:12 +1000

    day 15, i'd like to optimise a little further but no point.

--- aoc15.py
+++ aoc15.py
@@ -1,7 +1,19 @@
+import re
 import sys
 
-answer = None
+answer = set()
+beacons = set()
+q = 2000000
 for line in sys.stdin.readlines():
   line = line.rstrip()
-
-print(answer)
+  sx, sy, bx, by = map(int, re.match(r'Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)', line).groups())
+  if by == q:
+    beacons.add(bx)
+  d = abs(sx - bx) + abs(sy - by)
+  dr = d - abs(sy - q) + 1
+  if dr <= 0:
+    continue
+  for i in range(sx - dr + 1, sx + dr):
+    answer.add(i)
+    
+print(len(answer - beacons))
