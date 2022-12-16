01aae5a6b2d3e16ed6e7d1e3d9196d6144bee036
Douglas Catchpole <>
2022-12-16 21:17:42 +1000

    day 16, reck'd. cubics factorials. eugh.

--- aoc15-2.py
+++ aoc15-2.py
@@ -1,3 +1,5 @@
+import cProfile
+
 import re
 import sys
 from collections import defaultdict
@@ -8,27 +10,33 @@ width = 4000001
 def md(x1, y1, x2, y2):
   return abs(x1 - x2) + abs(y1 - y2)
 
-ss = []
-for line in sys.stdin.readlines():
-  line = line.rstrip()
-  sx, sy, bx, by = map(int, re.match(r'Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)', line).groups())
-  ss.append((sx, sy, md(sx, sy, bx, by)))
 
-x = y = 0
-while x < width:
-  y = 0
-  while y < width:
-    # should have some sort of trie structure to limit the sensors to affected rows
-    for sx, sy, r in ss:
-      d = md(x, y, sx, sy)
-      if d <= r:
-        dx = abs(x - sx)
-        y = sy + (r - dx)
-        break
-    else:
-      print(x, y)
-      print(4000000*x+y)
-      sys.exit()
-        
-    y += 1
-  x += 1
+def findAnswer():
+  ss = []
+  for line in sys.stdin.readlines():
+    line = line.rstrip()
+    sx, sy, bx, by = map(int, re.match(r'Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)', line).groups())
+    ss.append((sx, sy, md(sx, sy, bx, by)))
+
+  x = y = 0
+  while x < width:
+    y = 0
+    while y < width:
+      # should have some sort of trie structure to limit the sensors to affected rows
+      for sx, sy, r in ss:
+        d = md(x, y, sx, sy)
+        if d <= r:
+          dx = abs(x - sx)
+          y = sy + (r - dx)
+          break
+      else:
+        print(x, y)
+        print(4000000*x+y)
+        return
+          
+      y += 1
+    x += 1
+
+with cProfile.Profile() as pr:
+  findAnswer()
+pr.print_stats()
--- aoc16.py
+++ aoc16.py
@@ -1,7 +1,100 @@
+import cProfile
+import functools
+import queue
+import re
 import sys
+import collections
+
+vertices = {}
+start = None
+target = None
+
+class Node(object):
+
+  def __init__(self, r, i):
+    self.r = r
+    self.id = i
+    self.edges = []
+
+  def add(self, v):
+    self.edges.append(v)
+
+  def value(self, t):
+    return self.r * t
+
+  def __str__(self):
+    return (f'{chr(self.id)}: [{[chr(e.e) for e in self.edges]}]')
 
 answer = None
-for line in sys.stdin.readlines():
+lines = sys.stdin.readlines()
+for line in lines:
   line = line.rstrip()
+  name, rate, output = re.match(r'Valve (.*) has flow rate=([0-9]+); tunnels? leads? to valves? (.*)', line).groups()
+  vertices[name] = Node(int(rate), name)
 
-print(answer)
+for line in lines:
+  line = line.rstrip()
+  name, rate, output = re.match(r'Valve (.*) has flow rate=([0-9]+); tunnels? leads? to valves? (.*)', line).groups()
+  v = vertices[name]
+  for out in output.split(', '):
+    v.add(vertices[out])
+
+
+def shortestPath(graph, source):
+  dist = {}
+  prev = {}
+  q = queue.PriorityQueue()
+  processed = set()
+  for i, v in graph.items():
+    dist[i] = 9999
+    if v == source:
+      dist[i] = 0
+    prev[i] = None
+    q.put((dist[i], i))
+
+  while not q.empty():
+    _, u = q.get()
+    if u in processed:
+      continue
+    processed.add(u)
+    for v in graph[u].edges:
+      if v.id not in processed:
+        alt = dist[u] + 1
+        if alt < dist[v.id]:
+          dist[v.id] = alt
+          prev[v.id] = u
+          q.put((alt, v.id))
+    
+  return dist, prev
+
+
+distances = {}
+paths = {}
+for i, node in vertices.items():
+  dist, prev = shortestPath(vertices, node)
+  distances[i] = dist
+  paths[i] = prev
+
+print(' \t' + '\t'.join(sorted(vertices)))
+for k in sorted(vertices):
+  print(f'{k}\t' + '\t'.join([str(distances[k][i]) for i in vertices]))
+
+
+off = tuple(sorted({v for v, n in vertices.items() if n.r}))
+
+@functools.cache
+def bestScore(at, t, off):
+  s = vertices[at].value(t)
+  if not off or t <= 0:
+    return s
+
+  ss = 0 
+  nxtOff = tuple(n for n in off if n != at)
+  for nxt in nxtOff:
+    nxt_score = bestScore(nxt, t - 1 - distances[at][nxt], nxtOff)
+    ss = max(ss, nxt_score)
+
+  return s + ss
+
+print(max(bestScore(x, 29 - distances['AA'][x], off) for x in off))
+  
