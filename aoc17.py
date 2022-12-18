b62f97999c2563107c749adffad270b18c00c69e
Douglas Catchpole <>
2022-12-18 21:19:42 +1000

    both 17 and 18 (i) were trivial. (ii)s have been caught overcomplicating things.

--- aoc17.py
+++ aoc17.py
@@ -1,7 +1,65 @@
 import sys
 
-answer = None
-for line in sys.stdin.readlines():
-  line = line.rstrip()
+shapes = [[(0, 0), (0, 1), (0, 2), (0, 3)],
+          [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)],
+          [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],
+          [(0, 0), (1, 0), (2, 0), (3, 0)],
+          [(0, 0), (0, 1), (1, 0), (1, 1)]]
+shifts = sys.stdin.read().strip()
+n_shifts = len(shifts)
+print(n_shifts)
 
-print(answer)
+height = 0
+i = 0
+shiftPos = 0
+grid = [
+  [0] * 7,
+  [0] * 7,
+  [0] * 7,
+  [0] * 7,
+  [0] * 7,
+  [0] * 7,
+  [0] * 7,
+]
+
+def blocked(shape, x, y):
+  for off_y, off_x in shape:
+    if x < 0 or y < 0 or (x + off_x > 6) or grid[y + off_y][x + off_x]:
+      return True
+  return False
+
+def printGrid():
+  for line in grid[::-1]:
+    print('|' + ''.join(['#' if s else ' ' for s in line]) + '|')
+  print('+-------+')
+
+been = set()
+heights = []
+while i < 2755:
+  #printGrid()
+  x, y = 2, height + 3
+  shape = shapes[i%5]
+  if (i%5, shiftPos) in been:
+    print(i, i%5, shiftPos, height)
+  else:
+    been.add((i%5, shiftPos))
+  i += 1
+  while True:
+    shift = shifts[shiftPos]
+    shiftPos = (shiftPos+1)%n_shifts
+    if shift == '<' and not blocked(shape, x - 1, y):
+      x -= 1
+    elif shift == '>' and not blocked(shape, x + 1, y):
+      x += 1
+
+    if not blocked(shape, x, y - 1):
+      y -= 1
+    else:
+      height = max(height, y + shape[-1][0] + 1)
+      while len(grid) < height + 7:
+        grid.append([0] * 7)
+      for off_y, off_x in shape:
+        grid[y + off_y][x + off_x] = 1
+      break
+
+print(height)
--- aoc18.py
+++ aoc18.py
@@ -1,7 +1,54 @@
+import queue
 import sys
+import collections
 
 answer = None
-for line in sys.stdin.readlines():
-  line = line.rstrip()
+faces = set()
+faceCount = collections.defaultdict(int)
+drops = set()
+dim = 0
 
-print(answer)
+def add(c):
+  faces.add(c)
+  faceCount[c] += 1
+
+def surrounds(cube):
+  x, y, z = cube
+  yield (x+1, y, z)
+  yield (x-1, y, z)
+  yield (x, y+1, z)
+  yield (x, y-1, z)
+  yield (x, y, z+1)
+  yield (x, y, z-1)
+
+for line in sys.stdin.readlines():
+  x, y, z = map(int, line.rstrip().split(','))
+  dim = max(dim, x, y, z)
+  drops.add((x,y,z))
+  for s in surrounds((x,y,z)):
+    add(s)
+
+openCubes = faces - drops
+openFaces = sum(faceCount[c] for c in openCubes)
+print(openFaces)
+
+visited = set()
+q = queue.Queue()
+q.put((-1, -1, -1))
+res = 0
+while not q.empty():
+  c = q.get()
+  visited.add(c)
+  
+  for n in surrounds(c):
+    if min(n) < -2 or max(n) > dim + 2:
+      continue
+    if n in visited:
+      continue
+    if n in drops:
+      res += 1
+    else:
+      visited.add(n)
+      q.put(n)
+
+print(res)
